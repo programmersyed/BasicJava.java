d1ea5d84d236d28edd2970746702862e4a446d46
programmersyed <abrahamsyed10@gmail.com>
2022-06-28 13:53:59 -0400

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,14 @@
+// Define your tree class in this file!
+public class Tree{ 
+  public static void main(String[] args){//There will be a tree
+System.out.println("Hey there, I’m Ibrahim and I’m learning to code in Java!");
+System.out.println("Ready to get my hands dirty!");
+System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  "); 
+
+}
+
+}
